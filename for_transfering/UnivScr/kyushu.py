ce88a00a5da36587f212559cad1ab8a05d1b8d1b
[ishiyeahman] <ishiyeahman@gmail.com>
2021-09-04 15:10:42 +0900

    could get url

--- for_transfering/UnivScr/kyushu.py
+++ for_transfering/UnivScr/kyushu.py
@@ -2,9 +2,10 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-load_url = 'https://www.kyushu-u.ac.jp/ja/'
+load_url = 'https://www.kyushu-u.ac.jp/ja/topics/' #'https://www.kyushu-u.ac.jp/ja/'
+base = 'https://www.kyushu-u.ac.jp'
 
-
+"""
 def get(): 
     univ_info = list()
     univ_info_time = list()
@@ -24,9 +25,39 @@ def get():
             univ_info_time += [time.text]
             
     return univ_info, univ_info_time 
+"""
+
+def get(): 
+    univ_info = list()
+    univ_info_time = list()
+    url = list()
+
+    html = requests.get(load_url)
+    soup = BeautifulSoup(html.content, "html.parser")
+
+    # for element in soup.find_all(class_="main").find_all(class_="a_new"):  
+    #     print(element.text)
         
+    body = soup.find_all(class_="txtarea")
+    body = body[1]
+
+    #body = soup.select(".txtarea").text
+    
+    for cell in body.find_all('dl'):
+        for info in cell.find_all('a'):  
+            univ_info += [info.text]
+            url += [base+ info.get('href')]
+        for time in cell.find_all('dt'):  
+            univ_info_time += [time.text]
+        
+        for link in cell.find_all('a'):  
+           url += [base+ link.get('href')]
+    
+    return univ_info, univ_info_time, url
+   
 if __name__ == "__main__":
     get()
+    #print(get())
     
     
     
--- for_transfering/main.py
+++ for_transfering/main.py
@@ -4,15 +4,20 @@ import UnivScr.kyushu as kyushu
 import UnivScr.kumamoto as kumamoto
 
 univ_name = ['kyushu','kumamoto']
-num_info = 10
+num_info = 3
 
 def call():
     for name in univ_name :
-        info, date = eval( name +'.get()')
-        
+        print(name + '--------------')
+        info, date , url= eval( name +'.get()')
+
         #数字のみに正規化
         date = norm_dt(date)
+        #info = norm_dt(info)
 
+
+        #debag
+        """
         # num_infoの数のみ出力
         if ( len(info) > num_info -1 ) and ( len(date) > num_info -1 ) :
             for i in range(num_info):
@@ -20,7 +25,8 @@ def call():
         else :
             for i in range(min( len(info), len(date))):
                 print(date[i] + ' : ' + info[i])
-
+        """
+        
 def norm_dt(dt):
     num = []
     for d in dt:
@@ -28,6 +34,17 @@ def norm_dt(dt):
     
     return num
 
+def norm_info(info):
+    txt = []
+    pattern=r"\s+"
+    for i in info:
+        print(i)
+        txt += [re.sub(pattern, "", i)]
+        
+    return txt
+
+def upload():
+    print()
 
 if __name__ == '__main__':
     call();
